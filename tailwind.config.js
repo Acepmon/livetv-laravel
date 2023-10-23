4433e128ec284ffcbe76bd35e56150128e418aef
acepmon <dtsogtbayar123@gmail.com>
2023-10-23 15:14:42 +0800

    filament form builder installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,10 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+import preset from './vendor/filament/support/tailwind.config.preset'
 
-module.exports = {
-    darkMode: 'class',
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
-    variants: {
-        extend: {
-            backgroundColor: ['active'],
-        }
-    },
+export default {
+    presets: [preset],
     content: [
-        './app/**/*.php',
-        "./resources/**/*.{php,html,js,jsx,ts,tsx,vue,twig}",
-    ],
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
+        './app/Filament/**/*.php',
+        './resources/views/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
     ],
 }
