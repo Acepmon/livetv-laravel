99d7ec9d5a056d49850935676efe8836d17d2c62
acepmon <dtsogtbayar123@gmail.com>
2023-11-03 04:10:28 +0800

    restructured and transition added

--- resources/views/components/ui/frontend/home-nav-link.blade.php
+++ resources/views/components/ui/frontend/home-nav-link.blade.php
@@ -1,30 +1,10 @@
 @props([
-    'type' => 'default', 
-    'size' => 'md', 
-    'tag' => 'button',
-    'href' => '/',
-    'submit' => false,
-    'rounded' => 'full'
+    'href' => '/'
 ])
 
-@php
-switch ($tag ?? 'button') {
-    case 'button':
-        $tagAttr = ($submit) ? 'button type="submit"' : 'button type="button"';
-        $tagClose = 'button';
-        break;
-    case 'a':
-        $link = $href ?? '';
-        $tagAttr = 'a  href="' . $link . '"';
-        $tagClose = 'a';
-        break;
-    default:
-        $tagAttr = 'button type="button"';
-        $tagClose = 'button';
-        break;
-}
-@endphp
-
-<{!! $tagAttr !!} {!! $attributes->except(['class']) !!} class="text-gray-700 hover:bg-gray-200/75 w-full group flex items-center gap-x-4 rounded-lg px-2 py-1 sm:py-2 lg:py-3 text-base leading-6 sm:leading-8 font-semibold">
+<a 
+    {{ $attributes }} class="text-gray-700 transition-colors duration-200 ease-out bg-transparent hover:bg-gray-200/75 w-full group flex items-center gap-x-4 rounded-lg px-2 py-1 sm:py-2 lg:py-3 text-base leading-6 sm:leading-8 font-semibold" 
+    href="{{ $href }}"
+>
     {{ $slot }}
-</{{ $tagClose }}>
+</a>
